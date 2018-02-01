836c49ac057038d853c74888ca991b068f195b1f
Parker Higgins <parker@parkerhiggins.net>
2018-02-01 00:38:48 -0500

    added PDF (but not yet author page) support for the toast)

--- grabber.js
+++ grabber.js
@@ -3,7 +3,13 @@ const argv = require('minimist')(process.argv.slice(2));
 
 const url = argv.url;
 const outdir = argv.outdir || '.';
-let offset = url.indexOf('.com');
+if (url.includes('.com')) {
+    offset = url.indexOf('.com');
+} else if (url.includes('.net')) {
+    offset = url.indexOf('.net');
+} else {
+    offset = 3;
+}
 
 let filename = url.substring(offset + 5).replace(/\//g,'-');
 
@@ -11,13 +17,26 @@ if (filename.endsWith('.php')) {
     filename = filename.slice(0,-4);
 }
 
+if (filename.endsWith('-')) {
+    filename = filename.slice(0,-1);
+}
+
 (async () => {
     const browser = await puppeteer.launch({ignoreHTTPSErrors:true});
     const page = await browser.newPage();
+
+    /* await page.setJavaScriptEnabled(false); */
+
     await page.setExtraHTTPHeaders({'Accept-Charset': 'utf-8'});
     
     try {
-        await page.goto(url, {'waitUntil':'networkidle'});
+        const res = await page.goto(url, {'waitUntil':'networkidle'});
+        if (res.ok !== true) {
+            console.log('Server returned status code ' + res.code);
+            process.exitCode = 1;
+            await browser.close();
+            return;
+        }
     } catch (e) {
         console.log(e);
         process.exitCode = 1;
@@ -25,20 +44,33 @@ if (filename.endsWith('.php')) {
         return;
     }
 
+    await page.emulateMedia('screen');
+
+    let pdf_options = {displayHeaderFooter: true, margin: {top: '.5in', bottom: '.5in', left: '.5in', right: '.5in'}, printBackground: true, path: outdir + '/' + filename + '.pdf'}
+
     if (url.includes('dnainfo.com')) {
-        await page.addStyleTag({path: 'dnainfotweaks.css'})
+        await page.addStyleTag({path: 'tweaks/dnainfo.css'})
     }
 
-    await page.emulateMedia('screen');
-	try {
-    await page.pdf({displayHeaderFooter: true, margin: {top: '.5in', bottom: '.5in', left: '.5in', right: '.5in'}, printBackground: true, path: outdir + '/' + filename + '.pdf'});
-	} catch (e) {
+
+    if (url.includes('laweekly.com')) {
+        pdf_options.printBackground = false;
+        await page.addStyleTag({path: 'tweaks/laweekly.css'})
+    }
+
+    if (url.includes('the-toast.net')) {
+        await page.addStyleTag({path: 'tweaks/thetoast.css'})
+		await page.setViewport({width:500, height:600});
+    }
+
+    try {
+    await page.pdf(pdf_options);
+    } catch (e) {
         console.log(e);
         process.exitCode = 1;
         await browser.close();
         return;
     }
-
 
     await browser.close();
 })();
